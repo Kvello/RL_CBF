29b13c04440e969b62e89805dbe28b20152c2424
Kvello <markus.kv1@gmail.com>
2024-11-27 10:14:09 +0100

    Remove curriculum

--- src/RL_CBF/config/task_config/CBF_navigation_task_config.py
+++ src/RL_CBF/config/task_config/CBF_navigation_task_config.py
@@ -30,7 +30,7 @@ class task_config:
     robot_name = "CBF_quadrotor"
     controller_name = "lee_velocity_control"
     args = {}
-    num_envs = 265
+    num_envs = 256
     use_warp = True
     headless = True
     device = "cuda:0"
@@ -38,17 +38,17 @@ class task_config:
         "height": 16,
         "width": 64,
     }
-    CBF_safe_dist = CBFLidarConfig.min_range + 0.5
-    cbf_kappa_gain = 0.5
+    CBF_safe_dist = CBFLidarConfig.min_range + 0.4
+    cbf_kappa_gain = 1.5
     plot_cbf_constraint = True
     penalize_cbf_constraint = False
     penalize_cbf_corrections = False
-    filter_actions = True
+    filter_actions = False
     max_speed = 2.0 # m/s
     max_yawrate = 20.0*torch.pi/180.0 # rad/s max yawrate
     max_angle_of_attack = CBFLidarConfig.vertical_fov_deg_max*torch.pi/180.0 # rad max angle of attack
     # Luckily our LiDAR has symmetric vertical field of view, so we don't need a min value
-    observation_space_dim = 13 + 4 + vae_config.latent_dims + 1 # root state + action_dim _ + downsampled_lidar_dims + z-position
+    observation_space_dim = 13 + 4 + vae_config.latent_dims # root state + action_dim _ + lidar_vae_latent_dim
     privileged_observation_space_dim = 0
     action_space_dim = 4
     episode_len_steps = 150  # real physics time for simulation is this value multiplied by sim.dt
@@ -83,15 +83,15 @@ class task_config:
         "yawrate_absolute_action_penalty_magnitude": 0.2,
         "yawrate_absolute_action_penalty_exponent": 2.0,
         "collision_penalty": -100.0,
-        "cbf_correction_penalty_magnitude" : -20.0,
-        "cbf_invariance_penalty_magnitude" : 20.0,
+        "cbf_correction_penalty_magnitude" : -5.0,
+        "cbf_invariance_penalty_magnitude" : 5.0,
     }
 
     class curriculum:
         min_level = 10
         max_level = 45
         check_after_log_instances = 2048
-        increase_step = 2
-        decrease_step = 1
+        increase_step = 0
+        decrease_step = 0
         success_rate_for_increase = 0.8
         success_rate_for_decrease = 0.6
